fde83d860ee1d3bf0d905842388b9748742e71c8
anastasiiastarostenko <anastasiia@mppteam.com>
2021-12-20 22:45:23 +0200

    Added task for module 6-1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,40 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.render();
+  }
+  
+  render() {
+    this.elem = document.createElement('TABLE');
+    this.elem.innerHTML = `<thead>
+                            <tr>
+                              <th>Имя</th>
+                              <th>Возраст</th>
+                              <th>Зарплата</th>
+                              <th>Город</th>
+                              <th></th>
+                            </tr>
+                          </thead>
+                          <tbody>`;
+
+    this.elem.innerHTML += this.rows.map(item =>
+    `<tr><td>${item.name}</td>
+        <td>${item.age}</td>
+        <td>${item.salary}</td>
+        <td>${item.city}</td>
+        <td><button class = 'remove'>X</button></td>
+      </tr>`
+      ).join('');
+
+      this.elem.innerHTML += '</tbody>';
+      this.elem.addEventListener('click', this.onClick);
+  
+      return this.elem;
+  }
+
+  onClick = (ev) => {
+    if (ev.target.tagName != 'BUTTON') return;
+    ev.target.parentElement.parentElement.remove();
   }
 }
